8250604e05b697b1f86269c0d97aca299d78479f
S.KORT <air@MacBook-Air.local>
2026-09-12 19:13:58 +0100

    Augmente le timeout de init-coolpay-payment à 30s

--- amplify/functions/init-coolpay-payment/resource.ts
+++ amplify/functions/init-coolpay-payment/resource.ts
@@ -4,6 +4,7 @@ export const initCoolPayPayment = defineFunction({
   name: 'init-coolpay-payment',
   entry: './handler.ts',
   resourceGroupName: 'data',
+  timeoutSeconds: 30,
   environment: {
     MYCOOLPAY_PUBLIC_KEY: secret('MYCOOLPAY_PUBLIC_KEY'),
     MYCOOLPAY_PRIVATE_KEY: secret('MYCOOLPAY_PRIVATE_KEY'),
